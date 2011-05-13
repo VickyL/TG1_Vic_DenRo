843dde46a24d4bd725336ba93d1eceff03545187
Victoria <vicky_vj16@hotmail.com>
2011-05-13 07:19:27 -0600

    actualizado

---
+++ src/trabajo_grupo1/Cuota.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trabajo_grupo1;
+
+/**
+ *
+ * @author ViCoOo
+ */
+public class Cuota {
+    public double pagoMensual;
+    public boolean pagada;
+
+    public Cuota(double pago, boolean p){
+        pagoMensual=pago;
+        pagada=p;
+    }
+}
